ed8d7320bfc761fd37b7ba13054929da421c3e63
mayk3l <michal.larysz91@gmail.com>
2021-10-04 14:52:14 +0200

    adding if else condition

--- routes/user.js
+++ routes/user.js
@@ -11,45 +11,46 @@ router.post('/register', isRegistered, async (req, res) => {
    try {
        if (req.user) {
            res.status(200).json(req.user);
-       }
-       const { first_name, last_name, email, pwd } = req.body;
+       } else {
+           const { first_name, last_name, email, pwd } = req.body;
 
-       if (!(email && pwd && first_name && last_name)) {
-           res.status(400).send('fill_all_required_form_fields');
-       }
-
-       // check if user already exist
-
-       const exists = await User.findOne({ email });
-       if (exists) {
-           return res.status(409).send("user_exists");
-       }
-
-       //Encrypt user password
-       const encryptedPassword = await bcrypt.hash(pwd, 10);
-
-       // Create user in our database
-       const user = await User.create({
-           first_name,
-           last_name,
-           email: email.toLowerCase(), // sanitize: convert email to lowercase
-           pwdHash: encryptedPassword,
-           updateToken: uuid.v4(),
-       });
-
-       // Create token
-       const token = jwt.sign(
-           { user_id: user._id, email },
-           'HEALFYKEY',
-           {
-               expiresIn: config.maxAge,
+           if (!(email && pwd && first_name && last_name)) {
+               res.status(400).send('fill_all_required_form_fields');
            }
-       );
-       // save user token
-       user.token = token;
 
-       // return new user
-       res.status(201).json(user);
+           // check if user already exist
+
+           const exists = await User.findOne({ email });
+           if (exists) {
+               return res.status(409).send("user_exists");
+           }
+
+           //Encrypt user password
+           const encryptedPassword = await bcrypt.hash(pwd, 10);
+
+           // Create user in our database
+           const user = await User.create({
+               first_name,
+               last_name,
+               email: email.toLowerCase(), // sanitize: convert email to lowercase
+               pwdHash: encryptedPassword,
+               updateToken: uuid.v4(),
+           });
+
+           // Create token
+           const token = jwt.sign(
+               { user_id: user._id, email },
+               'HEALFYKEY',
+               {
+                   expiresIn: config.maxAge,
+               }
+           );
+           // save user token
+           user.token = token;
+
+           // return new user
+           res.status(201).json(user);
+       }
    } catch (error) {
        console.log(error);
    }
